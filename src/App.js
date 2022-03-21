f9d1a785ae7d93b13d3bbcce4b77bf7582a98e97
Aditya Raj Singh <aditya.cena007@gmail.com>
2022-03-22 01:05:03 +0530

    my first commit Reactwebsite

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import TodoList from './component/TodoList';
+//import TodoForm from './component/TodoForm';
+
+function App() {
+  return (
+    <div className="todo-app">
+    
+    <TodoList />
+
+    </div>
+  );
+}
+
+export default App;
